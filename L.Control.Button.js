c4d25256c2aa41ec46ca8cec4ae4752de1a62391
Maxim Dolgov <bananafishbone@gmail.com>
2015-11-19 16:37:05 +0300

    reformatting to tabs

--- L.Control.Button.js
+++ L.Control.Button.js
@@ -22,83 +22,83 @@
  */
 
 L.Control.Button = L.Control.extend({
-    includes: L.Mixin.Events,
-    options: {
-        position: 'topleft'
-    },
-    initialize: function (body, options) {
-        L.setOptions(this, options);
-        var button;
+	includes: L.Mixin.Events,
+	options: {
+		position: 'topleft'
+	},
+	initialize: function (body, options) {
+		L.setOptions(this, options);
+		var button;
 
-        button = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom button-control');
-        if (body instanceof HTMLElement) {
-            button.appendChild(body);
-        } else {
-            button.innerHTML = this._prepareBody(body);
-        }
-        L.DomUtil.addClass(button, this.options.position);
+		button = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom button-control');
+		if (body instanceof HTMLElement) {
+			button.appendChild(body);
+		} else {
+			button.innerHTML = this._prepareBody(body);
+		}
+		L.DomUtil.addClass(button, this.options.position);
 
-        this._container = button;
+		this._container = button;
 
-        return this;
-    },
+		return this;
+	},
 
-    _prepareBody: function (body) {
-        var code = body.replace(/^\s+|\s+$/, '');
-        if (code.charAt(0) !== '<') {
-            code = '<a href="javascript:;">' + code + '</a>';
-        }
-        return code;
-    },
+	_prepareBody: function (body) {
+		var code = body.replace(/^\s+|\s+$/, '');
+		if (code.charAt(0) !== '<') {
+			code = '<a href="javascript:;">' + code + '</a>';
+		}
+		return code;
+	},
 
-    isToggled: function () {
-        return L.DomUtil.hasClass(this._container, this.options.toggleButton);
-    },
-    _fireClick: function (e) {
-        this.fire('click');
+	isToggled: function () {
+		return L.DomUtil.hasClass(this._container, this.options.toggleButton);
+	},
+	_fireClick: function (e) {
+		this.fire('click');
 
-        if (this.options.toggleButton) {
-            var btn = this._container;
-            if (this.isToggled()) {
-                L.DomUtil.removeClass(this._container, this.options.toggleButton);
-            } else {
-                L.DomUtil.addClass(this._container, this.options.toggleButton);
-            }
-        }
-    },
-    onAdd: function (map) {
-        if (this._container) {
-            L.DomEvent.on(this._container, 'click', this._fireClick, this);
-            var stop = L.DomEvent.stopPropagation;
-            L.DomEvent.on(this._container, 'mousedown', stop)
-                .on(this._container, 'touchstart', stop)
-                .on(this._container, 'dblclick', stop)
-                .on(this._container, 'mousewheel', stop)
-                .on(this._container, 'MozMozMousePixelScroll', stop);
-            this.fire('load');
+		if (this.options.toggleButton) {
+			var btn = this._container;
+			if (this.isToggled()) {
+				L.DomUtil.removeClass(this._container, this.options.toggleButton);
+			} else {
+				L.DomUtil.addClass(this._container, this.options.toggleButton);
+			}
+		}
+	},
+	onAdd: function (map) {
+		if (this._container) {
+			L.DomEvent.on(this._container, 'click', this._fireClick, this);
+			var stop = L.DomEvent.stopPropagation;
+			L.DomEvent.on(this._container, 'mousedown', stop)
+				.on(this._container, 'touchstart', stop)
+				.on(this._container, 'dblclick', stop)
+				.on(this._container, 'mousewheel', stop)
+				.on(this._container, 'MozMozMousePixelScroll', stop);
+			this.fire('load');
 
-            this._map = map;
-        }
+			this._map = map;
+		}
 
-        return this._container;
-    },
-    onRemove: function (map) {
-        if (this._container && this._map) {
-            L.DomEvent.off(this._container, 'click', this._fireClick, this);
-            L.DomEvent.off(this._container, 'mousedown', stop)
-                .off(this._container, 'touchstart', stop)
-                .off(this._container, 'dblclick', stop)
-                .off(this._container, 'mousewheel', stop)
-                .off(this._container, 'MozMozMousePixelScroll', stop);
+		return this._container;
+	},
+	onRemove: function (map) {
+		if (this._container && this._map) {
+			L.DomEvent.off(this._container, 'click', this._fireClick, this);
+			L.DomEvent.off(this._container, 'mousedown', stop)
+				.off(this._container, 'touchstart', stop)
+				.off(this._container, 'dblclick', stop)
+				.off(this._container, 'mousewheel', stop)
+				.off(this._container, 'MozMozMousePixelScroll', stop);
 
-            this.fire('unload');
-            this._map = null;
-        }
+			this.fire('unload');
+			this._map = null;
+		}
 
-        return this;
-    }
+		return this;
+	}
 });
 
 L.control.button = function (label, options) {
-    return new L.Control.Button(label, options);
+	return new L.Control.Button(label, options);
 };
